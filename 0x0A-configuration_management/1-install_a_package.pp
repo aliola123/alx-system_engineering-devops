fb9e02790be60a525b5c83e9067b28c6c6630040
aliola123 <alipoemoses6461@gmail.com>
2023-12-28 23:57:03 +0100

    minor change

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
+##!/usr/bin/pup
 #This manifest installs flask from pip
-exec {'flask':
-  command => '/usr/bin/pip3 install flask',
-  path    => '/usr/bin',
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
